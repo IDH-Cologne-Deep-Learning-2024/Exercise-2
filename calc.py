0d78177fa4dce0597b7c3ab8a93ada3b12b53fae
Jago- <Jago-@192.168.2.163>
2024-10-17 14:20:02 +0200

    basic menu done

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
 def calc():
+    print('select mathematical operator')
+    print('adition')
     print("Enter number 1: ")
     num1 = int(input())
     print("Enter number 2: ")
@@ -6,13 +8,19 @@ def calc():
     print("Sum: " + str(num1+num2))
     print("Product: " + str(num1*num2))
 
-print('Run program calc in interactive (i) or in batch (b) mode.')
-choice = str(input())
-if choice == 'i':
-    print('running interactive mode')
-    calc()
-elif choice == 'b':
-    print ('running batch mode')
-else:
-    print('no matching input')
-    calc.py
+def calcb():
+    print('test')
+
+def main():
+    print('Run program calc in interactive (i) or in batch (b) mode.')
+    choice = str(input())
+    if choice == 'i':
+        print('running interactive mode')
+        calc()
+    elif choice == 'b':
+        print ('running batch mode')
+        calcb()
+    else:
+        print('no matching input')
+        main()
+main()
