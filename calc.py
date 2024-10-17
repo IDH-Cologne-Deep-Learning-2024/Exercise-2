7eaf0ec2c602617a0ca54bb7c030535b00eec646
Jago- <Jago-@192.168.2.163>
2024-10-17 14:54:03 +0200

    first task done

--- calc.py
+++ calc.py
@@ -1,24 +1,69 @@
 def calc():
     print('select mathematical operator')
-    print('adition')
-    print("Enter number 1: ")
-    num1 = int(input())
-    print("Enter number 2: ")
-    num2 = int(input())
-    print("Sum: " + str(num1+num2))
-    print("Product: " + str(num1*num2))
+    print('addition (+) multiplication (*)')
+
+    operator = str(input())
+    if operator == '+':
+
+        print('addition: \n')
+        x = num_input()
+        add(x[0],x[1])
+        print('\ncontinue addition? yes (y)')
+        t = str(input())
+        if t == 'y':
+            x = num_input()
+            add(x[0],x[1])
+        else: 
+            print('stopping operartion')
+
+    elif operator == '*':
+        print('muliplication: \n')
+        x = num_input()
+        mult(x[0],x[1])
+        print('\ncontinue muliplication? yes (y)')
+        t = str(input())
+        if t == 'y':
+            x = num_input()
+            mult(x[0],x[1])
+        else: 
+            print('stopping operartion')
+
+    else:
+        print('no matching input')
+    
+    print('\ncontinue mathematical operation with diferent operator? yes (y)')
+    t = str(input())
+    if t == 'y':
+        calc()
+    else:
+        print('stopping operartion')
+        exit()
 
 def calcb():
     print('test')
+
+def add(num1, num2):
+    print("Sum: " + str(num1+num2))
+
+def mult(num1, num2):
+    print("Product: " + str(num1*num2))
+
+def num_input():
+    num_input_array = [0,0]
+    print("Enter number 1: ")
+    num_input_array[0] = int(input())
+    print("Enter number 2: ")
+    num_input_array[1] = int(input())
+    return num_input_array
 
 def main():
     print('Run program calc in interactive (i) or in batch (b) mode.')
     choice = str(input())
     if choice == 'i':
-        print('running interactive mode')
+        print('running interactive mode: \n')
         calc()
     elif choice == 'b':
-        print ('running batch mode')
+        print ('running batch mode: \n')
         calcb()
     else:
         print('no matching input')
