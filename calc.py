dd376e0c0021267b98995190a969731a8da1d8b1
NP <ninasopopp@gmail.com>
2024-10-20 14:42:49 +0200

    feature 1 added functions add and multiply

--- calc.py
+++ calc.py
@@ -3,8 +3,16 @@ def calc():
     num1 = int(input())
     print("Enter number 2: ")
     num2 = int(input())
-    print("Sum: " + str(num1+num2))
-    print("Product: " + str(num1*num2))
+    print("Sum: " + add(num1, num2))
+    print("Product: " + multiply(num1, num2))
+
+def add(num1, num2):
+    sum = str(num1 + num2)
+    print(sum)
+
+def multiply(num1, num2):
+    product = str(num1 * num2)
+    print(product)
 
 print('Run program calc in interactive (i) or in batch (b) mode.')
 choice = str(input())
