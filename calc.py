3d1ca5df211b107a2e8397d44178d7d5354c6144
Janis Pagel <janis.pagel@uni-koeln.de>
2024-10-24 10:13:39 +0200

    Revert to commit a1991098416b07e43452b486489cd501386b620c

--- calc.py
+++ calc.py
@@ -1,18 +1,6 @@
-def calc():
-    print("Enter number 1: ")
-    num1 = int(input())
-    print("Enter number 2: ")
-    num2 = int(input())
-    print("Sum: " + str(num1+num2))
-    print("Product: " + str(num1*num2))
-
-print('Run program calc in interactive (i) or in batch (b) mode.')
-choice = str(input())
-if choice == 'i':
-    print('running interactive mode')
-    calc()
-elif choice == 'b':
-    print ('running batch mode')
-else:
-    print('no matching input')
-    calc.py
+print("Enter number 1: ")
+num1 = int(input())
+print("Enter number 2: ")
+num2 = int(input())
+print("Sum: " + str(num1+num2))
+print("Product: " + str(num1*num2))
